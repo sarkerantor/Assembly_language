60dffb784f0063117b0cc26296d5e0b407d7a538
Antor Sarker <antor.s.cse10.bu@gmail.com>
2026-09-01 01:20:50 +0600

    Create print_1toN.asm

---
+++ print_1toN.asm
@@ -0,0 +1,22 @@
+.MODEL SMALL
+.STACK 100H
+.DATA
+    N CX 5
+.CODE
+MAIN PROC
+    MOV AX, @DATA
+    MOV DS, AX
+
+    MOV CX, 5        ; Print 1 to 5
+    MOV DL, '1'
+
+PRINT_LOOP:
+    MOV AH, 02H
+    INT 21H
+    INC DL
+    LOOP PRINT_LOOP
+
+    MOV AH, 4CH
+    INT 21H
+MAIN ENDP
+END MAIN
