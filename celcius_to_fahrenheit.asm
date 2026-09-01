34b3e15391f01566c200b1d591f76998b7426d22
Antor Sarker <antor.s.cse10.bu@gmail.com>
2026-09-01 08:15:23 +0600

    Update celcius_to_fahrenheit.asm

--- celcius_to_fahrenheit.asm
+++ celcius_to_fahrenheit.asm
@@ -20,3 +20,61 @@ MAIN PROC
     INT 21H
 MAIN ENDP
 END MAIN
+
+
+
+
+
+.MODEL SMALL
+.STACK 100H
+.DATA
+    C DB 25
+    F DB ?
+    MSG DB 'Fahrenheit: $'
+
+.CODE
+MAIN PROC
+    MOV AX, @DATA
+    MOV DS, AX
+
+    ; Calculation: F = (C * 9 / 5) + 32
+    MOV AL, C
+    MOV BL, 9
+    MUL BL            ; AX = 25 * 9 = 225
+    MOV BL, 5
+    DIV BL            ; AL = 225 / 5 = 45
+    ADD AL, 32        ; AL = 45 + 32 = 77
+    MOV F, AL
+
+    ; Display Message
+    MOV AH, 09H
+    LEA DX, MSG
+    INT 21H
+
+    ; Print Number (Max 2 Digits: 0-99)
+    MOV AL, F
+    MOV AH, 0
+    MOV BL, 10
+    DIV BL            ; AL = Tens digit (7), AH = Units digit (7)
+
+    MOV BX, AX        ; Save result in BX
+
+    ; Print Tens Digit
+    MOV DL, BL
+    ADD DL, '0'       ; Convert to ASCII
+    MOV AH, 02H
+    INT 21H
+
+    ; Print Units Digit
+    MOV DL, BH
+    ADD DL, '0'       ; Convert to ASCII
+    MOV AH, 02H
+    INT 21H
+
+    ; Exit Program
+    MOV AH, 4CH
+    INT 21H
+MAIN ENDP
+END MAIN
+
+
